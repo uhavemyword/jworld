3af802a0e59dff0d7c3086f7fceaa3013c43ae0d
Ben <uhavemyword@users.noreply.github.com>
2018-05-06 12:51:51 +0800

    display username

--- frontend/src/app/components/_layouts/app-header/app-header.component.ts
+++ frontend/src/app/components/_layouts/app-header/app-header.component.ts
@@ -4,4 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   templateUrl: './app-header.component.html'
 })
-export class AppHeaderComponent { }
+export class AppHeaderComponent {
+  username: string;
+
+  ngOnInit() {
+    this.username = localStorage.getItem('user');
+  }
+}
