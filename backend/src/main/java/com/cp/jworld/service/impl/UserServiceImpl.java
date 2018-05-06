e2417f7276ceea2cffc54565d84989178f4f9911
Ben <uhavemyword@users.noreply.github.com>
2018-05-06 15:23:12 +0800

    delete contact when delete user

--- backend/src/main/java/com/cp/jworld/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/cp/jworld/service/impl/UserServiceImpl.java
@@ -27,4 +27,11 @@ public class UserServiceImpl extends EntityServiceImpl<User, UserDao> implements
         contact.setUser(user);
         contactDao.insert(contact);
     }
+    
+    @Override
+    @Transactional
+    public void deleteById(Integer id) {
+        contactDao.deleteByUserId(id);
+        userDao.deleteById(id);
+    }
 }
