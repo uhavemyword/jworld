76be536106ab55b4fb50f0dd452eae807b925646
Ben <uhavemyword@users.noreply.github.com>
2018-05-07 01:10:02 +0800

    alter column

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  `url` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
 
 CREATE TABLE `contact` (
